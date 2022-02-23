67c65fded4884aaa6b1eef434b747525d0c1d50a
Saïd Mimouni <sayid.mimouni@gmail.com>
2022-02-23 11:46:07 +0100

    pokemons#show

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -2,4 +2,8 @@ class PokemonsController < ApplicationController
   def index
     @pokemons = Pokemon.all
   end
+
+  def show
+    @pokemon = Pokemon.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pokemons#index'
+  resources :pokemons
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
