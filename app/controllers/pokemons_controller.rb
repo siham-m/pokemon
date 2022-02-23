d96ea11c04741a78bd8d67d8f9627b6d68c540a5
Saïd Mimouni <sayid.mimouni@gmail.com>
2022-02-23 12:27:42 +0100

    pokemons#import

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -6,4 +6,9 @@ class PokemonsController < ApplicationController
   def show
     @pokemon = Pokemon.find(params[:id])
   end
+
+  def import
+    response = HTTParty.get('https://pokeapi.co/api/v2/pokemon')
+    puts response.body, response.code, response.message, response.headers.inspect
+  end
 end
