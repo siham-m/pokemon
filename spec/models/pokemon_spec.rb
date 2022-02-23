b8713f689c732175da0c83eee8ada20ebd82ce48
Saïd Mimouni <sayid.mimouni@gmail.com>
2022-02-23 10:52:08 +0100

    test factory bot

--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Pokemon, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   it 'has valid factory' do
     expect(FactoryBot.build(:pokemon)).to be_valid
     expect(FactoryBot.create(:pokemon)).to be_valid
