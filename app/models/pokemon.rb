4fea162cd5a11192ef71d846dbcb71ea5419edbe
Saïd Mimouni <sayid.mimouni@gmail.com>
2022-02-23 11:35:21 +0100

    test pokemon

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,5 +1,5 @@
 class Pokemon < ApplicationRecord
-  validates :name, uniqueness: true
+  validates :name, presence: true, uniqueness: true
   validates :height, presence: true
   validates :weight, presence: true
 end
--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -6,11 +6,27 @@ RSpec.describe Pokemon, type: :model do
     expect(FactoryBot.create(:pokemon)).to be_valid
   end
 
-  it 'has a name'
-
-  it 'has a unique name'
-
-  it 'has a weight'
-
-  it 'has a height'
+  it 'has a name' do
+    expect do
+      FactoryBot.create(:pokemon, name: nil)
+    end.to raise_error(ActiveRecord::RecordInvalid)
+  end
+  it 'has a unique name' do
+    expect do
+      FactoryBot.create(:pokemon)
+    end.not_to raise_error
+    expect do
+      FactoryBot.create(:pokemon)
+    end.to raise_error(ActiveRecord::RecordInvalid)
+  end
+  it 'has a weight' do
+    expect do
+      FactoryBot.create(:pokemon, weight: nil)
+    end.to raise_error(ActiveRecord::RecordInvalid)
+  end
+  it 'has a height' do
+    expect do
+      FactoryBot.create(:pokemon, height: nil)
+    end.to raise_error(ActiveRecord::RecordInvalid)
+  end
 end
