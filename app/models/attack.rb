a2fa83f78ad1f32e7e1c28d066c32b0efcaf7cd3
Saïd Mimouni <sayid.mimouni@gmail.com>
2022-02-23 19:37:16 +0100

    add validation to attack model accuracy/power

--- app/models/attack.rb
+++ app/models/attack.rb
@@ -1,5 +1,5 @@
 class Attack < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  validates :accuracy, presence: true
-  validates :power, presence: true
+  validates :accuracy, presence: true, numericality: true
+  validates :power, presence: true, numericality: true
 end
