41b396c6fef3f2eafb4385f4e86a40f2e67be96b
Saïd Mimouni <sayid.mimouni@gmail.com>
2022-02-23 10:45:20 +0100

    test

--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Pokemon, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory'
+
+  it 'has a name'
+
+  it 'has a unique name'
+
+  it 'has a weight'
+  
+  it 'has a height'
 end
