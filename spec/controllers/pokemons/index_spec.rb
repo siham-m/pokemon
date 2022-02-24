97c503dae0c5b343606c7f73404a42e9f571dd87
Saïd Mimouni <sayid.mimouni@gmail.com>
2022-02-24 12:57:43 +0100

    re-organize tests

--- spec/controllers/pokemons/index_spec.rb
+++ spec/controllers/pokemons/index_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe PokemonsController, type: :controller do
-  it 'displays :index' do
+  context 'when pokemon is not found' do
+    it 'raises error' do
+    end
+  end
+  context 'when pokemon is found' do
+    it 'displays :index' do
       get :index
       expect(response).to render_template(:index)
-  end
-  it 'assigns pokemons' do
-    pokemon1 = FactoryBot.create(:pokemon)
-    pokemon2 = FactoryBot.create(:pokemon)
-    get :index
-    expect(assigns(:pokemons)).to eq([pokemon1, pokemon2])
+    end
+    it 'assigns pokemons' do
+      pokemon1 = FactoryBot.create(:pokemon)
+      pokemon2 = FactoryBot.create(:pokemon)
+      get :index
+      expect(assigns(:pokemons)).to eq([pokemon1, pokemon2])
+    end
   end
 end
--- spec/controllers/pokemons/show_spec.rb
+++ spec/controllers/pokemons/show_spec.rb
@@ -1,15 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe PokemonsController, type: :controller do
-  it 'displays :show' do
+  context 'when pokemon is not found' do
+    it 'raises error' do
+      get :show, params: {id: 'test'}
+      expect(response.status).to eq(404)
+    end
+  end
+
+  context 'when pokemon is found' do
+    it 'displays :show' do
       pokemon = FactoryBot.create(:pokemon)
       get :show, params: {id: pokemon.name}
       expect(response).to render_template(:show)
-  end
-
-  it 'assigns pokemon' do
-    pokemon = FactoryBot.create(:pokemon)
-    get :show, params: {id: pokemon.name}
-    expect(assigns(:pokemon)).to eq(pokemon)
+    end
+    it 'assigns pokemon' do
+      pokemon = FactoryBot.create(:pokemon)
+      get :show, params: {id: pokemon.name}
+      expect(assigns(:pokemon)).to eq(pokemon)
+    end
   end
 end
