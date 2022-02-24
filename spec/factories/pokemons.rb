d44c472ecfcc934cc384164ff6b494a9cb3b59fc
Saïd Mimouni <sayid.mimouni@gmail.com>
2022-02-24 14:48:13 +0100

    Fix factories

--- spec/factories/pokemons.rb
+++ spec/factories/pokemons.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :pokemon do
-    name { Faker::Games::Pokemon.name }
+    name { Faker::Games::Pokemon.name.parameterize }
     height{ 40 }
     weight{ 6 }
     picture{ "https://youtube.com" }
