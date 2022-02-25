94e40dc40c90b58cb79498547ed779056e7d875a
Saïd Mimouni <sayid.mimouni@gmail.com>
2022-02-25 12:52:17 +0100

    add pagination cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,10 @@
 class CardsController < ApplicationController
   def index
     @cards = Card.all
+    respond_to do |format|
+      format.html do
+        @cards = @cards.paginate(page: params[:page], per_page: 50)
+      end
+    end
   end
 end
